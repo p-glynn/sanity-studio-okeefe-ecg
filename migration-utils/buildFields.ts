4ad8dede4aaede2b4b69b7d260581d97e3fdf787
Patrick Glynn <pglynnco@gmail.com>
2024-09-23 15:35:56 -0600

    move types & utils to own files

--- migration-utils/buildFields.ts
+++ migration-utils/buildFields.ts
@@ -1,46 +1,6 @@
-import {readFile, writeFile} from 'fs/promises';
-import {join} from 'path';
-
-interface InputData {
-    id: string;
-    label: string;
-}
-
-interface Group extends InputData {
-    data: InputData[];
-}
-
-interface Field {
-    name: string;
-    type: string;
-    title: string;
-    fieldset: string;
-}
-
-interface Fieldset {
-    name: string;
-    title: string;
-    options: {collapsible: boolean; collapsed: boolean};
-}
-
-/**
- * Write data to its own JSON file.
- * @param {Object} inputData - JSON data to write to file
- * @param {string} subdirectory - name of the subdirectory to write to
- * @param {string} fileName - name of the file
- */
-async function writeData(inputData: object, subdirectory: string, fileName: string) {
-    const filePath = join('output', subdirectory, `${fileName}.json`);
-    console.log(`Writing ${filePath}...`);
-
-    const dataToWrite = JSON.stringify(inputData, null, 2);
-    try {
-        await writeFile(filePath, dataToWrite);
-        console.log(`Finished writing ${filePath}`);
-    } catch (error) {
-        console.error(`Error writing file: ${error}`);
-    }
-}
+import {readFile} from 'fs/promises';
+import {writeData} from './writeData';
+import {Group, InputData, Field, Fieldset} from './types/types';
 
 async function main() {
     const args = process.argv.slice(2); // Ignore the first two arguments (node and script path)
