ced85407e6f94ad4a916449aae937c8121a98612
Patrick Glynn <pglynnco@gmail.com>
2024-09-24 15:16:14 -0600

    delete output file before beginning process, update file paths

--- migration-utils/buildNdJson.ts
+++ migration-utils/buildNdJson.ts
@@ -1,17 +1,16 @@
-import {readFile, appendFile} from 'fs/promises';
+import {readFile, appendFile, unlink} from 'fs/promises';
 import {stringify} from 'ndjson';
 
 async function main() {
     const args = process.argv.slice(2);
     const inputType = args[0];
-    const path = `questions/test/${inputType}`;
-    const file = await readFile(`input/${path}.json`, 'utf8');
+    const file = await readFile(`input/questions/processed/${inputType}.json`, 'utf8');
     const {data} = JSON.parse(file);
-
+    await unlink(`output/questions/${inputType}.ndjson`);
     const stream = stringify();
     stream.on('data', async function (line: string) {
         // line is a line of stringified JSON with a newline delimiter at the end
-        await appendFile(`output/${path}.ndjson`, line);
+        await appendFile(`output/questions/${inputType}.ndjson`, line);
     });
     data.forEach((question: any) => stream.write(question));
     stream.end();
