f3701f51d1ff16724a3db5f47792ebf19b2cd51f
Patrick Glynn <pglynnco@gmail.com>
2024-10-01 11:15:46 -0600

    add try/catch & log at end

--- migration-utils/buildNdJson.ts
+++ migration-utils/buildNdJson.ts
@@ -6,13 +6,18 @@ async function main() {
     const inputType = args[0];
     const file = await readFile(`input/questions/processed/${inputType}.json`, 'utf8');
     const {data} = JSON.parse(file);
-    await unlink(`output/questions/${inputType}.ndjson`);
+    try {
+        await unlink(`output/questions/${inputType}.ndjson`);
+    } catch (error) {
+        console.log('No file to delete');
+    }
     const stream = stringify();
     stream.on('data', async function (line: string) {
         // line is a line of stringified JSON with a newline delimiter at the end
         await appendFile(`output/questions/${inputType}.ndjson`, line);
     });
     data.forEach((question: any) => stream.write(question));
+    console.log(`Finished writing to file ${inputType}.ndjson`);
     stream.end();
 }
 
