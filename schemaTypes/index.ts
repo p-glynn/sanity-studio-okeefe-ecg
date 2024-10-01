d22c8a624c42842dba6f986d98bb7e1578822c29
Patrick Glynn <pglynnco@gmail.com>
2024-10-01 11:18:44 -0600

    add qbank to index

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import ecg from './ecg';
 import echo from './echo';
 import angiogram from './angiogram';
+import qbank from './qbank';
 
-export const schemaTypes = [ecg, echo, angiogram];
+export const schemaTypes = [ecg, echo, angiogram, qbank];
